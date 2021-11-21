713f0a8bb75d48a6e93a50568407e6ba538db3a5
Gwennaël Arbona <gwennael.arbona@gmail.com>
2021-11-21 10:33:50 +0100

    Prevent docking while on trajectory

--- Source/AstralShipwright/UI/Menu/NovaMainMenuFlight.cpp
+++ Source/AstralShipwright/UI/Menu/NovaMainMenuFlight.cpp
@@ -280,7 +280,13 @@ bool SNovaMainMenuFlight::IsUndockEnabled() const
 
 bool SNovaMainMenuFlight::IsDockEnabled() const
 {
-	return IsValid(PC) && IsValid(SpacecraftMovement) && SpacecraftMovement->CanDock();
+	if (OrbitalSimulation)
+	{
+		const FNovaTrajectory* PlayerTrajectory = OrbitalSimulation->GetPlayerTrajectory();
+		return PlayerTrajectory == nullptr && IsValid(PC) && IsValid(SpacecraftMovement) && SpacecraftMovement->CanDock();
+	}
+
+	return false;
 }
 
 bool SNovaMainMenuFlight::IsManeuveringEnabled() const
