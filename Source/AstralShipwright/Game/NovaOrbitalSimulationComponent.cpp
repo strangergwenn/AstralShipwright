82e501f2a3a9f434225e4fe3fa43dcb6b1a069d7
Gwennaël Arbona <gwennael.arbona@gmail.com>
2022-01-04 19:21:15 +0100

    Remove dead code

--- Source/AstralShipwright/Game/NovaOrbitalSimulationComponent.cpp
+++ Source/AstralShipwright/Game/NovaOrbitalSimulationComponent.cpp
@@ -27,14 +27,9 @@ struct FNovaHohmannTransfer
 	{
 		const double SourceSemiMajorAxis = 0.5f * (ManeuverRadius + OriginalRadius);
 
-#if 1
 		// StartDeltaV = VTransfer1 - VSource
 		StartDeltaV = sqrt((2.0 * µ * DestinationRadius) / (ManeuverRadius * (ManeuverRadius + DestinationRadius))) -
 					  sqrt(µ * ((2.0 / ManeuverRadius) - (1.0 / SourceSemiMajorAxis)));
-#else
-		// StartDeltaV = VTransfer1 - VSource, factorized with VSource
-		StartDeltaV = sqrt(µ / ManeuverRadius) * (sqrt((2.0 * DestinationRadius) / (ManeuverRadius + DestinationRadius)) - 1.0);
-#endif
 
 		// EndDeltaV = VDest - VTransfer2
 		EndDeltaV = sqrt(µ / DestinationRadius) * (1.0 - sqrt((2.0 * ManeuverRadius) / (ManeuverRadius + DestinationRadius)));
