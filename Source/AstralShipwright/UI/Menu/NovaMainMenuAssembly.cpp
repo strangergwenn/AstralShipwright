648d2cd8d62f3dab541cdfe2b48c9925c99a4ad8
Gwennaël Arbona <gwennael.arbona@gmail.com>
2022-06-19 17:11:25 +0200

    Remove hotkey for the photo mode

--- Source/AstralShipwright/UI/Menu/NovaMainMenuAssembly.cpp
+++ Source/AstralShipwright/UI/Menu/NovaMainMenuAssembly.cpp
@@ -902,10 +902,9 @@ void SNovaMainMenuAssembly::Construct(const FArguments& InArgs)
 						.AutoWidth()
 						[
 							SNeutronAssignNew(PhotoModeButton, SNeutronButton)
-							.Action(FNeutronPlayerInput::MenuAltPrimary)
 							.Text(LOCTEXT("PhotoMode", "Toggle photo mode"))
 							.HelpText(LOCTEXT("PhotoModeHelp", "Hide the interface to show off your spacecraft"))
-							.OnClicked(this, &SNovaMainMenuAssembly::OnEnterPhotoMode, FNeutronPlayerInput::MenuAltPrimary)
+							.OnClicked(this, &SNovaMainMenuAssembly::OnEnterPhotoMode, FNeutronPlayerInput::MenuConfirm)
 						]
 
 						+ SHorizontalBox::Slot()
